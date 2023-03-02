7f1144f94f78fc11ab33f7e9f3e30dd9d18c18bd
Martin Maiorano <martinmaiorano32@gmail.com>
2023-03-02 10:06:43 -0300

    d

--- Ejemplos/CV-Per.js
+++ Ejemplos/CV-Per.js
@@ -1,30 +1,37 @@
-const obj = '{"name":{"first":"Martin","last":"Maiorano"},"email":"martinmaiorano32@gmail.com","location":{"country":"Argentina","city":"Mar del plata","street":{"name":"*******", "number":"----"}},"date":"1995-11-14","cell":"(223)668-9376","login":{"title":"Argentina programa 4.0","descript":"En curso"},"picture":"Img/CV.png"}';
-const cv = JSON.parse(obj)
-console.log(cv);
+let cv = {}
+function rand(){
+    fetch("https://randomuser.me/api/?inc=gender,name,nat,picture,email,cell,picture,dob,location,login")
+    .then((response) => response.json())
+    .then(function(data){
+        cv = data.results[0]
+        lista.forEach((item)=>
+        item.classList.remove("activar"))
+        document.getElementsByClassName("list")[0].classList.add("activar")
+        let btn = document.getElementsByTagName("a")
+        document.getElementsByTagName("img")[0].src = cv.picture.large
+        document.getElementById("Texto").innerHTML = cv.name.first+" "+cv.name.last
 
-let btn = document.getElementsByTagName("a")
-document.getElementsByTagName("img")[0].src = cv.picture
-document.getElementById("Texto").innerHTML = cv.name.first+" "+cv.name.last
-
-btn[0].addEventListener("click",function(){
-    document.getElementById("Texto").innerHTML = cv.name.first+" "+cv.name.last
-})
-btn[1].addEventListener("click",function(){
-    document.getElementById("Texto").innerHTML = cv.email
-})
-btn[2].addEventListener("click",function(){
-    let aux = new Date(cv.date)
-    document.getElementById("Texto").innerHTML = aux.getDate() + "/" + aux.getMonth() + "/" + aux.getFullYear()
-})
-btn[3].addEventListener("click",function(){
-    document.getElementById("Texto").innerHTML = "Pais: "+cv.location.country+"<br>Ciudad: "+cv.location.city+"<br>Direccion: "+cv.location.street.name+" "+cv.location.street.number
-})
-btn[4].addEventListener("click",function(){
-    document.getElementById("Texto").innerHTML = cv.cell
-})
-btn[5].addEventListener("click",function(){
-    document.getElementById("Texto").innerHTML = "Lugar: "+cv.login.title+"<br>Estado: "+cv.login.descript
-})
+        btn[0].addEventListener("click",function(){
+            document.getElementById("Texto").innerHTML = cv.name.first+" "+cv.name.last
+        })
+        btn[1].addEventListener("click",function(){
+            document.getElementById("Texto").innerHTML = cv.email
+        })
+        btn[2].addEventListener("click",function(){
+            let aux = new Date(cv.dob.date)
+            document.getElementById("Texto").innerHTML = aux.getDate() + "/" + aux.getMonth() + "/" + aux.getFullYear()
+        })
+        btn[3].addEventListener("click",function(){
+            document.getElementById("Texto").innerHTML = "Pais: "+cv.location.country+"<br>Ciudad: "+cv.location.city+"<br>Direccion: "+cv.location.street.name+" "+cv.location.street.number
+        })
+        btn[4].addEventListener("click",function(){
+            document.getElementById("Texto").innerHTML = cv.cell
+        })
+        btn[5].addEventListener("click",function(){
+            document.getElementById("Texto").innerHTML = "Usuario: "+cv.login.username+"<br>Contraseña: "+cv.login.password
+        })
+    })
+}
 let lista = document.querySelectorAll(".list")
 function activar(){
     lista.forEach((item)=>
@@ -33,3 +40,4 @@ function activar(){
 }
 lista.forEach((item)=>
 item.addEventListener("click",activar))
+rand()
